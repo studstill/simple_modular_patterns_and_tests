84794b7274888130ae2a6528f1b8ecc5256144be
Kevin Smith <kamil@sisna.com>
2016-03-01 10:18:34 -0800

    Initial Commit

---
+++ test/greet_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect =  require('chai').expect;
+var greetObject = require(__dirname + '/../greet.js');
+
+describe('testing functions in greet.js', function(){
+    it('Should return "hello Kevin"', function(){
+        expect(greetObject('Kevin')).to.equal('hello Kevin');
+    });
+});

97d83a96fd6490b4270039b2ec6c74ec6f3baf5a
Mikleane Chouinard <mikleane@msn.com>
2016-03-01 12:15:19 -0800

    greet and test

---
+++ mikleane01/greet.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function greet(name) {
+  if (process.argv.length <= 2) {
+  return "Hello, " + name + '!';
+} else {
+  return "Hello, " + process.argv[2];
+}
+};
+module.exports = greet;
---
+++ mikleane01/test/sayhello-test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var expect = require('chai').expect;
+var hello = require(__dirname + '/../greet');
+
+
+describe ('testing greet function from greet.js', function() {
+  it('greet should return "Hello, Kitty!"', function(){
+    expect(hello('Kitty')).to.equal("Hello, Kitty!");
+  });
+});
+
+describe ('testing that terminal will accept a variable and output string', function(){
+  it('param should return process.argv[2]', function(){
+    var param = process.argv[2];
+    expect(param).to.equal(process.argv[2]);
+  });
+});
+
+describe('testing the if else argument', function(){
+  it('if process.argv.length is <=2 return greeting', function(){
+    var greeting = hello('Sunshine');
+    expect(greeting).to.equal("Hello, Sunshine!");
+  })
+});

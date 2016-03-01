9235ba527c59f04d80e984c5215b0dc901b559b6
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-01 12:26:11 -0800

    add part of bonus

--- Samantha/hello.js
+++ Samantha/hello.js
@@ -1,5 +1,21 @@
 'use strict';
 
 var greet = require (__dirname + '/greet');
-return(greet('Samantha'));
-console.log(greet('Samantha'));
+// console.log(greet('Samantha'));
+// return(greet('Samantha'));
+
+// if(process.argv.length >= 3){
+//   var param = process.argv[2];
+//   // console.log(process.argv)
+//   console.log(greet(param))
+// } else {
+//   console.log(greet('Samantha'))
+// }
+
+if(process.argv.length >= 4){
+  var param = process.argv[2];
+  // console.log(process.argv)
+  console.log(greet(param))
+} else {
+  console.log(greet('Samantha'))
+}

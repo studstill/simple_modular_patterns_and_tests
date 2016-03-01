56a2ce6076f8a17ada3e5e0108b535f714e052b1
Sawako Ishida <sawako.yoriko@gmail.com>
2016-02-29 21:03:25 -0800

    test passing with greet function

---
+++ sawako-lib/greet.js
@@ -0,0 +1,6 @@
+'use strict';
+exports = module.exports = {};
+
+exports.greet = function (name){
+  return "hello " + name;
+};
---
+++ test/greet-test.js
@@ -0,0 +1,9 @@
+'use strict';
+var expect = require('chai').expect;
+var greet = require('../lib/greet.js');
+
+describe('testing greet function from greet.js', function(){
+  it('greet function should return "hello, Sawako"', function(){
+    expect(greet('Sawako')).to.equal("hello, Sawako");
+  });
+});

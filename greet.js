29c4671e9cef9f6c614b85c84f95460ce1bbd06a
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-03-01 17:53:04 -0800

    revised greet. weird error when passing default into gulp

--- greet.js
+++ greet.js
@@ -1,15 +1,5 @@
 'use strict'
-var nodeName = process.argv[2];
-if (nodeName){
-  greet(nodeName);
-}
 
-function greet(name) {
-  if(name){
-    console.dir('hello, ' + name);
-  }
-
+var greet = module.exports = function(name) {
   return 'hello, ' + name + '!';
-}
-
-module.exports = greet;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var mocha = require('gulp-mocha');
 var paths = ['*.js', 'test/*.js'];
 
 gulp.task('lint', function(){
@@ -9,3 +10,10 @@ gulp.task('lint', function(){
     .pipe(eslint())
     .pipe(eslint.format());
 });
+
+gulp.task('mocha', function(){
+  return gulp.src('test/testGreet.js')
+    .pipe(mocha())
+});
+
+gulp.task('default', ['mocha', 'lint']);

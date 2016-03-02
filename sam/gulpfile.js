183f0003c7bf8d03581782b32f8cb153154c4cd0
Sam Gruse <sgruse89@gmail.com>
2016-03-02 10:18:00 -0800

    final hw/Lab for Wednesday March 2nd with stretch goal

--- sam/gulpfile.js
+++ sam/gulpfile.js
@@ -8,8 +8,48 @@ var path = ['*.js', 'lib/greet.js', 'test/*.js'];
 
 gulp.task('lint', function(){
   return gulp.src(path)
-    .pipe(lint())
-    .pipe(lint.format());
+    .pipe(lint({
+        'rules': {
+          'no-console': 0,
+          'indent': [
+            2,
+            2
+          ],
+          'quotes': [
+            2,
+            'single'
+          ],
+          'linebreak-style': [
+            2,
+            'unix'
+              ],
+              'semi': [
+                2,
+                'always'
+              ]
+        },
+        'env': {
+          'es6': true,
+          'node': true,
+          'browser': true
+        },
+        'globals': {
+          'describe': false,
+          'it': false,
+          'beforeEach': false,
+          'afterEach': false,
+          'before': false,
+          'after': false
+        },
+        'ecmaFeatures': {
+          'modules': true,
+          'experimentalObjectRestSpread': true
+        },
+        'extends': 'eslint:recommended'
+      }
+    ))
+    .pipe(lint.formatEach('compact',
+    process.stderr));
 });
 
 gulp.task('test', function(){
@@ -18,7 +58,7 @@ gulp.task('test', function(){
 });
 
 gulp.task('watch', function(){
-  gulp.watch('/../*.js', ['lint', 'test']);
+  gulp.watch(path, ['test', 'lint']);
 });
 
 gulp.task('default', ['lint', 'test', 'watch']);

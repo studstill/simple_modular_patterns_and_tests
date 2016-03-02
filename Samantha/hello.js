1597b4312000969ae38535a529b80194e04bcc82
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-02 08:55:47 -0800

    add bonus part 1

--- Samantha/hello.js
+++ Samantha/hello.js
@@ -4,18 +4,22 @@ var greet = require (__dirname + '/greet');
 // console.log(greet('Samantha'));
 // return(greet('Samantha'));
 
+
+var hello = function(){
+  var greeting = greet(process.argv[2] || 'world' );
+  console.log(greeting);
+  return greeting;
+};
+
+hello();
+
+module.exports = hello;
+
 // if(process.argv.length >= 3){
+//   //add for loop here
 //   var param = process.argv[2];
 //   // console.log(process.argv)
-//   console.log(greet(param))
+//   console.log(greet(param));
 // } else {
-//   console.log(greet('Samantha'))
+//   console.log(greet('Samantha'));
 // }
-
-if(process.argv.length >= 4){
-  var param = process.argv[2];
-  // console.log(process.argv)
-  console.log(greet(param))
-} else {
-  console.log(greet('Samantha'))
-}

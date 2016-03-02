72d129d2cb60333bdaeead841a4816c6aa8bd2b4
samanthaprince928@gmail.com <samanthaprince928@gmail.com>
2016-03-02 11:04:02 -0800

    removed bonus for eslint

--- Samantha/gulpfile.js
+++ Samantha/gulpfile.js
@@ -12,46 +12,7 @@ gulp.task('tasks running', function(){
 
 gulp.task('eslint', function(){
   return gulp.src(paths)
-    .pipe(eslint({
-      'rules': {
-        'no-console': 0,
-        'indent': [
-          2,
-          2
-        ],
-        'quotes': [
-          2,
-          'single'
-        ],
-        'linebreak-style': [
-          2,
-          'unix'
-        ],
-        'semi': [
-          2,
-          'always'
-        ]
-      },
-      'env': {
-        'es6': true,
-        'node': true,
-        'browser': true
-      },
-      'globals': {
-        'describe': false,
-        'it': false,
-        'beforeEach': false,
-        'afterEach': false,
-        'before': false,
-        'after': false
-      },
-      'ecmaFeatures': {
-        'modules': true,
-        'experimentalObjectRestSpread': true
-      },
-      'extends': 'eslint:recommended'
-    }
-))
+    .pipe(eslint())
     .pipe(eslint.format());
 });
 
